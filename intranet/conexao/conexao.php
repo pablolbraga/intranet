6294fa96bafae53a49b9adf0d6b020720187941c
Pablo <pablolbraga@gmail.com>
2023-02-25 17:52:38 -0300

    Configuração Inicial e Conexao com o BD

---
+++ intranet/conexao/conexao.php
@@ -0,0 +1,15 @@
+<?php
+
+date_default_timezone_set("America/Sao_Paulo");
+
+class Conexao{
+
+    public function abrirConexao(){
+
+        try{
+            $pdo = new PDO("oci:dbname=192.168.0.3:1521/SRID;charset=AL32UTF8","dbIwSaudeResid", "x");
+        } catch(Exception $ex){
+            echo "Não foi possível conectar com o banco de dados. Erro: " . $ex->getMessage();
+        }
+    }
+}
