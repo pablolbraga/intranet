53f75509f8e011e3a60b2b5bc5264ff45d17df26
Pablo <pablolbraga@gmail.com>
2023-02-25 19:11:56 -0300

    Criação da Tela de Login

---
+++ intranet/pages/index.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+
+require_once("../conexao/conexao.php");
+$conn = new Conexao();
+$pdo = $conn->abrirConexao();
+
+
+$_SESSION["LOGIN_USUARIO"] = "plima";
+?>
+<html>
+    <head>
+        <title>Saúde Residence - Intranet</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="../css/bs/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+    </head>
+    <body>
+        
+        <script src="../js/bs/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    </body>
+</html>
---
+++ intranet/pages/login.php
@@ -0,0 +1,42 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="../css/login.css">
+    </head>
+    <body>
+        <section class="ftco-section">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-7 col-lg-5">
+                        <div class="wrap">
+                            <br>
+                            <div class="container">
+                                <img src="../imgs/logo_completa.png" width="100%"/>
+                            </div>
+                            <div class="login-wrap p-4 p-md-5">
+                                <form action="#" class="signin-form">
+                                    <div class="form-group mt-3">
+                                        <input type="text" class="form-control" id="login" name="login" required>
+                                        <label class="form-control-placeholder" for="login">Login</label>                                        
+                                    </div>
+                                    <div class="form-group">
+                                        <input id="senha" name="senha" type="password" class="form-control" required>
+                                        <label class="form-control-placeholder" for="senha">Senha</label>
+                                        <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
+                                    </div>
+                                    <div class="form-group">
+                                        <button type="submit" class="form-control btn btn-primary rounded submit px-3">Acessar</button>
+                                    </div>
+                                    <div class="w-100 text-md-right">
+                                        <a href="#">Esqueceu a senha? Clique aqui.</a>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </body>
+</html>
